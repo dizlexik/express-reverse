8edde78f78dc99c27d61e88ed8e9920922c5b0ed
Alexandre Cisneiros <alexandre@cisneiros.com>
2014-06-19 21:34:35 -0300

    The returned URLs now consider the apps's mountpath, if there is one.

--- test/index.js
+++ test/index.js
@@ -4,11 +4,14 @@ var reverse = require('..');
 
 describe('express-reverse', function() {
   var app;
+  var mountedApp;
   var noop = function(req, res, next) { next(); };
 
   beforeEach(function() {
     app = express();
+    mountedApp = express();
     reverse(app);
+    reverse(mountedApp);
   });
 
   it('should augment express app', function() {
@@ -33,6 +36,21 @@ describe('express-reverse', function() {
     assert.throws(function() { url('test 4'); }, 'Route not found: test 4');
   });
 
+  it('should generate reverse route URLs considering mounted express apps', function() {
+    mountedApp.get('test 1', '/test', noop);
+    mountedApp.get('test 2', '/test/:x', noop);
+    mountedApp.get('test 3', '/test/:x?', noop);
+    app.use('/test-path', mountedApp);
+
+    var url = mountedApp.locals.url;
+    assert.equal(url('test 1'), '/test-path/test');
+    assert.equal(url('test 2', { x: '1' }), '/test-path/test/1');
+    assert.throws(function() { url('test 2'); }, 'Missing value for "x".');
+    assert.equal(url('test 3', { x: '1' }), '/test-path/test/1');
+    assert.equal(url('test 3'), '/test-path/test');
+    assert.throws(function() { url('test 4'); }, 'Route not found: test 4');
+  });
+
   it('should add res.redirectToRoute middleware', function() {
     var middelware;
     app.use = function(fn) { middleware = fn; };
@@ -54,5 +72,29 @@ describe('express-reverse', function() {
     assert.deepEqual(redirectArgs, [301, '/test']);
     res.redirectToRoute('test 2', { x: 1 });
     assert.deepEqual(redirectArgs, ['/test/1']);
+  });
+
+  it('should add res.redirectToRoute middleware considering mounted express apps', function() {
+    var middelware;
+    mountedApp.use = function(fn) { middleware = fn; };
+    reverse(mountedApp);
+    var redirectArgs;
+    var res = {
+      redirect: function() { redirectArgs = Array.prototype.slice.call(arguments, 0); }
+    };
+    middleware(null, res, function() {});
+
+    assert.isDefined(res.redirectToRoute);
+
+    mountedApp.get('test 1', '/test', noop);
+    mountedApp.get('test 2', '/test/:x', noop);
+    app.use('/test-path', mountedApp);
+
+    res.redirectToRoute('test 1');
+    assert.deepEqual(redirectArgs, ['/test-path/test']);
+    res.redirectToRoute(301, 'test 1');
+    assert.deepEqual(redirectArgs, [301, '/test-path/test']);
+    res.redirectToRoute('test 2', { x: 1 });
+    assert.deepEqual(redirectArgs, ['/test-path/test/1']);
   });
 });
